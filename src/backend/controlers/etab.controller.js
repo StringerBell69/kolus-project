d117922ad76135e19451f586596181adbb87aa5b
StringerBell69 <d.sumbo@eleve.leschartreux.net>
2024-05-22 18:11:01 +0200

    commint work at school little changes to controlers

---
+++ src/backend/controlers/etab.controller.js
@@ -0,0 +1,149 @@
+const fs = require('fs');
+const path = require('path');
+const pathToPublic = path.join(__dirname, '..','..');
+const dataPath = path.join(pathToPublic, 'MOCK_DATA.json');
+
+
+const readData = () => {
+    const data = fs.readFileSync(dataPath, 'utf8');
+    return JSON.parse(data);
+};
+
+const writeData = (data) => {
+    fs.writeFileSync(dataPath, JSON.stringify(data, null, 2), 'utf8');
+};
+
+
+exports.getEtabs =  (req, res)=>{
+    
+    const etabs =  readData();
+    res.status(200).json(etabs);
+};
+
+exports.getEtabById =  (req, res)=>{
+
+    
+    const etabs =  readData();
+    const etab = etabs.find(x => x.id ===parseInt(req.params.id));
+    if (etab) {
+        res.status(200).json(etab);
+    } else {
+        res.status(404).json({ message: "Établissement non trouvé" });
+    }
+};
+
+
+exports.getEtabByVille =  (req, res)=>{
+
+    const etabs =  readData()
+    const etab = etabs.filter(x => x.location === req.params.location);
+    if (etab) {
+        res.status(200).json(etab);
+    } else {
+        res.status(404).json({ message: "Établissement non trouvé" });
+    }
+};
+
+exports.getEtabByType =  (req, res)=>{
+
+    const etabs =  readData()
+    const etab = etabs.filter(x => x.etablissement_type === req.params.etablissement_type);
+    if (etab) {
+        res.status(200).json(etab);
+    } else {
+        res.status(404).json({ message: "Établissement non trouvé" });
+    }
+};
+
+
+exports.getEtabByTypeAndVille =  (req, res)=>{
+    
+    const etabs =  readData()
+    const etab = etabs.filter(x => x.etablissement_type === req.params.etablissement_type && x.location === req.params.location);
+    if (etab) {
+        res.status(200).json(etab);
+    } else {
+        res.status(404).json({ message: "Établissement non trouvé" });
+    }
+};
+
+
+
+
+
+exports.editEtab =  (req, res)=>{
+
+    const etabs = readData();
+    const index = etabs.findIndex(x => x.id === parseInt(req.params.id));
+    if (index !== -1) {
+        etabs[index] = { ...etabs[index], ...req.body };
+        writeData(etabs);
+        res.status(200).json(etabs[index]);
+    } else {
+        res.status(404).json({ message: "Établissement non trouvé" });
+    }
+};
+
+exports.deleteEtab =  (req, res)=>{
+
+    const etabs = readData();
+    const index = etabs.findIndex(x => x.id === parseInt(req.params.id));
+    if (index !== -1) {
+        etabs.splice(index, 1); 
+        writeData(etabs); 
+        res.status(200).json(etabs[index]);
+    } else {
+        res.status(404).json({ message: "Établissement non trouvé" });}; 
+    };
+
+exports.deleteEtabByVille =  (req, res)=>{
+
+    const etabs = readData();
+    const index = etabs.filter(x => x.id === req.params.location);
+    if (index !== -1) {
+        etabs.splice(index); 
+        writeData(etabs); 
+        res.status(200).json(etabs[index]);
+    } else {
+        res.status(404).json({ message: "Établissement non trouvé" });}; 
+    };
+
+
+exports.deleteEtabByType =  (req, res)=>{
+
+    
+    const etabs = readData();
+    const etab = etabs.filter(x => x.etablissement_type === req.params.etablissement_type);
+
+    const index = etab.filter(x => x.id === req.params.etablissement_type);
+    if (index !== -1) {
+        etabs.splice(index); 
+        writeData(etab); 
+        res.status(200).json(etab[index]);
+    } else {
+        res.status(404).json({ message: "Établissement non trouvé" });}; 
+    };
+
+
+exports.setEtabs =  (req, res)=>{
+
+    const etabs = readDatab();
+    const newEtab = {
+
+    id: req.body.id,
+
+    etablissement_type: req.body.etablissement_type,
+
+    etablissement: req.body.etablissement,
+
+    location:   req.body.location,
+
+    address: req.body.address,
+
+    mail:    req.body.mail,
+       
+    };
+    etabs.push(newEtab);
+    writeData(etabs);
+    res.status(200).json(newEtab);
+};
--- src/backend/index.js
+++ src/backend/index.js
@@ -1,4 +1,4 @@
-const express = require("express")
+const express = require("express");
 const port = 5000;
 const path = require('path');
 const app = express();
@@ -8,11 +8,18 @@ const pathToPublic = path.join(__dirname, '..','..','public');
 app.use(express.static(pathToPublic));
 
 
-app.get("/MOCK_DATA.json", (req, res) => {
-    res.sendFile(path.join(pathToPublic, 'MOCK_DATA.json'));
-});
+//  app.get("/MOCK_DATA.json", (req, res) => {
+//      res.sendFile(path.join(pathToPublic, 'MOCK_DATA.json'));
+//  });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+
+
+app.use("/etab" , require("./routes/etab.routes"));
 
 app.listen(port, ()=>{
     console.log("Serveur OK !")
-} )
+} );
 
