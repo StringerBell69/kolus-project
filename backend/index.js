2be49c97ebba2e4f07cf287b5e17a3fb4f308d22
StringerBell69 <d.sumbo@eleve.leschartreux.net>
2024-05-22 18:55:37 +0200

    files order

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const port = 5000;
+const path = require('path');
+const app = express();
+
+const pathToPublic = path.join(__dirname, '..','..','public');
+
+app.use(express.static(pathToPublic));
+
+
+//  app.get("/MOCK_DATA.json", (req, res) => {
+//      res.sendFile(path.join(pathToPublic, 'MOCK_DATA.json'));
+//  });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+
+
+app.use("/etab" , require("./routes/etab.routes"));
+
+app.listen(port, ()=>{
+    console.log("Serveur OK !")
+} );
+
---
+++ backend/routes/etab.routes.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const { setEtabs,getEtabById , getEtabs,getEtabByType,getEtabByVille ,getEtabByTypeAndVille,deleteEtabByType, deleteEtab ,editEtab } = require('../controlers/etab.controller');
+
+const app = express();
+
+app.get("/", (req, res) => {
+    getEtabs(req, res);  
+});
+
+app.get("/:id", (req, res) =>{
+    getEtabById(req, res); 
+});
+
+app.get("/ville/:location", (req, res) => {
+    getEtabByVille(req, res);
+});
+
+app.get("/type/:etablissement_type", (req, res) => {
+    getEtabByType(req, res);
+});
+
+app.get("/type/:etablissement_type/ville/:location", (req, res) => {
+    getEtabByTypeAndVille(req, res);
+});
+
+app.put("/:id", (req, res) => {
+    editEtab(req, res);
+});
+
+app.delete("/:id", (req, res) => {
+    deleteEtab(req, res);
+});
+
+app.delete("/ville/:location", (req, res) => {
+    deleteEtabByVille(req, res);
+});
+
+app.delete("/type/:etablissement_type", (req, res) => {
+    deleteEtabByType(req, res);
+});
+
+app.post("/", (req, res) => {
+    setEtabs(req, res);
+});
+
+
+
+
+
+
+
+module.exports = app;
